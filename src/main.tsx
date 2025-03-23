a54d336c2a5cf209a3de99bd38eb79c93a67c856
Kirill Larin <twenty29h@gmail.com>
2025-03-23 21:58:08 +0300

    setup msw

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,20 @@ import { createRoot } from 'react-dom/client';
 import { AppProvider } from '@/app/providers';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <AppProvider />
-  </StrictMode>
+async function enableMocking() {
+  if (!import.meta.env.DEV) {
+    return;
+  }
+
+  const { worker } = await import('./__mocks__/browser');
+
+  return worker.start();
+}
+
+enableMocking().then(() =>
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <AppProvider />
+    </StrictMode>
+  )
 );
