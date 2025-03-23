2ce00e79f4f87656d5ec83173afdd0dca92c6b91
Kirill Larin <twenty99h@DESKTOP-ECEJSDP.>
2025-03-23 14:35:20 +0300

    font setup

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,15 @@
 import './App.css';
 
 function App() {
-  return <div>BASE</div>;
+  return (
+    <div>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">The Joke Tax Chronicles</h1>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
+        Once upon a time, in a far-off land, there was a very lazy king who spent all day lounging on his throne. One
+        day, his advisors came to him with a problem: the kingdom was running out of money.
+      </p>
+    </div>
+  );
 }
 
 export default App;
